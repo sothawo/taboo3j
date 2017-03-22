3c5eb040aa2dc0083916e4e91e1c7a6fcb71a809
P.J. Meisch <pj.meisch@sothawo.com>
2017-03-22 16:08:10 +0100

    added AllControllers advice

--- src/main/java/com/sothawo/taboo3/mvc/HelloController.java
+++ src/main/java/com/sothawo/taboo3/mvc/HelloController.java
@@ -5,13 +5,10 @@ package com.sothawo.taboo3.mvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
 
 /**
  * dummy controller to get the program started.
@@ -20,16 +17,17 @@ import java.security.Principal;
  */
 @Controller
 @RequestMapping({"/", "/hello"})
-public class HelloController extends ControllerBase {
+public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    /**
+     * Username is added by the AllControllers advice.
+     *
+     * @return model and view
+     */
     @GetMapping
-    public ModelAndView hello(@AuthenticationPrincipal Principal principal) {
-        final ModelAndView mav = new ModelAndView("hello");
-
-        addUserNameToModel(principal, mav);
-
-        return mav;
+    public ModelAndView hello() {
+        return new ModelAndView("hello");
     }
 
 }
